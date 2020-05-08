3238c6d230819344052af01b2bc3c43b3fff849b
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-05-08 14:23:34 +0300

    finish day 19

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Volodymyr Ostapyshyn on 07.05.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        setBorderImage()
+        
+        askQuestion()
+        // Do any additional setup after loading the view.
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+    
+    fileprivate func setBorderImage() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+}
+

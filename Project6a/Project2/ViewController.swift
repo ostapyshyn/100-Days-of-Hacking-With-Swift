2e453076daf0120e6f55eb03632507fa37fcc376
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-05-15 13:49:47 +0300

    finish day 30

---
+++ Project6a/Project2/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Volodymyr Ostapyshyn on 07.05.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    var question = 0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        setBorderImage()
+        askQuestion()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        title = countries[correctAnswer].uppercased() + " Score: \(score)"
+    }
+    
+    fileprivate func setBorderImage() {
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        question += 1
+        print(question)
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        if question == 10 {
+            let ac = UIAlertController(title: "Result:", message: "Your final score is \(score).", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: nil))
+            question = 0
+            present(ac, animated: true)
+        }
+        
+        if sender.tag != correctAnswer {
+            let ac = UIAlertController(title: "Wrong!", message: "That’s the flag of \(sender.currentImage?.accessibilityIdentifier?.uppercased() ?? "0").", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
+    }
+    
+    @objc func shareTapped() {
+        let shareScore = "Your score is \(score)" //show and share
+        
+        let vc = UIActivityViewController(activityItems: [shareScore, " Plese share with your friends."], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
+

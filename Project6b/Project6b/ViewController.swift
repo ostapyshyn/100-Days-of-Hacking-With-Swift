8475d4b7e948db80122ae39fa04fdd80de9d075e
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-05-16 13:23:31 +0300

    Finish day 31

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         label1.translatesAutoresizingMaskIntoConstraints = false
         label1.backgroundColor = UIColor.red
         label1.text = "THESE"
+        label1.textAlignment = .center
         label1.sizeToFit()
 
         let label2 = UILabel()
@@ -49,12 +50,31 @@ class ViewController: UIViewController {
         view.addSubview(label4)
         view.addSubview(label5)
         
-        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
-        
-        for label in viewsDictionary.keys {
-            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        var previous: UILabel?
+
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+
+            if let previous = previous {
+                // we have a previous label – create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                // this is the first label
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+
+            // set the previous label to be the current one, for the next loop iteration
+            previous = label
         }
-        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        
+//        for label in viewsDictionary.keys {
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//        let metrics = ["labelHeight": 88]
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|-10-[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+        
     }
     
 }

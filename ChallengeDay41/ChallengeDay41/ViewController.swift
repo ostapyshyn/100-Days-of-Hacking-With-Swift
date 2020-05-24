2adcf6d01729cb63dcfee640f6c14eef789b3f52
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-05-24 18:31:26 +0300

    Add actions to buttons

--- ChallengeDay41/ChallengeDay41/ViewController.swift
+++ ChallengeDay41/ChallengeDay41/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     var submit: UIButton!
     var buttonsView: UIView!
     var guessesRemaining: UILabel!
+    var activatedButtons = [UIButton]()
+    var guessedWord: UILabel!
+    var solutions = [String]()
+    var usedLetters = [String]()
+    var round = 0
+    var lives = 7
+    
+    var wordToGuess: String!
+    var promptWord:String!
     
     override func loadView() {
         view = UIView()
@@ -28,11 +37,25 @@ class ViewController: UIViewController {
         loadLevel()
     }
     
+    func configureGuessedWord() {
+        guessedWord = UILabel()
+        guessedWord.translatesAutoresizingMaskIntoConstraints = false
+        guessedWord.textAlignment = .center
+        guessedWord.text = "???????"
+        guessedWord.font = UIFont.systemFont(ofSize: 48)
+        view.addSubview(guessedWord)
+        
+        NSLayoutConstraint.activate([
+            guessedWord.topAnchor.constraint(equalTo: guessesRemaining.bottomAnchor, constant: 24),
+            guessedWord.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
     func configureGuessesRemaining() {
         guessesRemaining = UILabel()
         guessesRemaining.translatesAutoresizingMaskIntoConstraints = false
         guessesRemaining.textAlignment = .center
-        guessesRemaining.text = "Guesses Remaining: 7"
+        guessesRemaining.text = "Guesses Remaining: \(lives)"
         guessesRemaining.font = UIFont.systemFont(ofSize: 48)
         view.addSubview(guessesRemaining)
         
@@ -48,6 +71,7 @@ class ViewController: UIViewController {
         configureClearButton()
         configureCurrentAnswer()
         configureGuessesRemaining()
+        configureGuessedWord()
     }
     
     func configureCurrentAnswer() {
@@ -70,13 +94,36 @@ class ViewController: UIViewController {
         
         let str = "abcdefghijklmnopqrstuvwxyz"
         let letters = Array(str.uppercased())
-        print(letters.count)
-        print(letterButtons.count)
-        
+        //print(letters.count)
+        //print(letterButtons.count)
+        solutions.removeAll()
         
         for i in 0 ..< letterButtons.count {
             self.letterButtons[i].setTitle(letters[i].description, for: .normal)
         }
+        
+        if let levelFileURL = Bundle.main.url(forResource: "Words", withExtension: "txt") {
+            if let levelContents = try? String(contentsOf: levelFileURL) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines.shuffle()
+                print(lines)
+                
+                for word in lines {
+                    solutions.append(word)
+
+                }
+            }
+        }
+        var count = solutions.count
+        print(solutions[round] + " solutions round + \(count)")
+        wordToGuess = solutions[round]
+        print(wordToGuess! + " from loadLevel")
+        promptWord = ""
+        for _ in 0..<solutions[round].count {
+            promptWord += "?"
+        }
+        print(promptWord!)
+        guessedWord.text = promptWord
         
         
     }
@@ -85,7 +132,7 @@ class ViewController: UIViewController {
         let clear = UIButton(type: .system)
         clear.translatesAutoresizingMaskIntoConstraints = false
         clear.setTitle("CLEAR", for: .normal)
-        //clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
+        clear.addTarget(self, action: #selector(clearTapped), for: .touchUpInside)
         clear.titleLabel?.font = UIFont.systemFont(ofSize: 24)
         view.addSubview(clear)
         
@@ -102,7 +149,7 @@ class ViewController: UIViewController {
         submit.setTitle("SUBMIT", for: .normal)
         submit.titleLabel?.font = UIFont.systemFont(ofSize: 24)
         
-        //submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
+        submit.addTarget(self, action: #selector(submitTapped), for: .touchUpInside)
         view.addSubview(submit)
         
         NSLayoutConstraint.activate([
@@ -156,6 +203,7 @@ class ViewController: UIViewController {
                 if letterButtons.count < 26 {
                     letterButtons.append(letterButton)
                     buttonsView.addSubview(letterButton)
+                    letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 }
                 
                 //letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
@@ -163,8 +211,84 @@ class ViewController: UIViewController {
         }
     }
     
+    @objc func letterTapped(_ sender: UIButton) {
+        guard let buttonTitle = sender.titleLabel?.text else { return }
+        currentAnswer.text = currentAnswer.text?.appending(buttonTitle)
+        activatedButtons.append(sender)
+        sender.isHidden = true
+    }
     
+    @objc func clearTapped() {
+        currentAnswer.text = ""
+        
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+
+        activatedButtons.removeAll()
+    }
     
+    @objc func submitTapped(_ sender: UIButton) {
+        guard currentAnswer.text?.count == 1 else {
+            showErrorMoreThanOne()
+            return
+        }
+        
+        guard let answerText = currentAnswer.text else { return }
+        
+        activatedButtons.removeAll()
+        usedLetters.append(answerText)
+        promptWord = ""
+        
+        if !wordToGuess.contains(answerText) {
+            lives -= 1
+            
+            guessesRemaining.text = "Guesses Remaining: \(lives)"
+            print(guessesRemaining.text!)
+            if lives == 0 {
+                showEndGameMessage()
+                loadLevel()
+                
+                return
+                
+            }
+        }
+        
+        
+
+        for letter in wordToGuess {
+            let strLetter = String(letter)
+
+            if usedLetters.contains(strLetter) {
+                promptWord += strLetter
+                
+            } else {
+                
+                promptWord += "?"
+            }
+            
+        }
+        
+        guessedWord.text = promptWord
+        
+        print(promptWord!)
+        clearTapped()
+        
+        
+        
+    }
     
+    func showErrorMoreThanOne() {
+        let ac = UIAlertController(title: "Incorrect guess", message: "Must be single letter!", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func showEndGameMessage() {
+        let ac = UIAlertController(title: "Incorrect guess", message: "You Lost The Game", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+
 }
 

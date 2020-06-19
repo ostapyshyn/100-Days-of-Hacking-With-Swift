1f4b485aa6a41c468955e71e1ed3bfe085612c61
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-19 23:53:19 +0300

    Add new project13 - Instafilter

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Volodymyr Ostapyshyn on 19.06.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+    @IBAction func intensityChanged(_ sender: UISlider) {
+        
+    }
+    @IBAction func changeFilter(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        
+    }
+}
+

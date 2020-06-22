3161b3eb03a62ffe9eac8910d653888a0ae4922f
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-22 14:53:18 +0300

    Finish day 54

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     @IBOutlet var intensity: UISlider!
     var currentImage: UIImage!
     var context: CIContext!
+    @IBOutlet var chageFilter: UIButton!
     var currentFilter: CIFilter!
+    
+    @IBOutlet var radius: UISlider!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +27,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
         
         context = CIContext()
+        chageFilter.setTitle("Change Filter (CISepiaTone)", for: .normal)
         currentFilter = CIFilter(name: "CISepiaTone")
         
     }
@@ -51,6 +57,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     @IBAction func intensityChanged(_ sender: UISlider) {
         applyProcessing()
     }
+    
+    @IBAction func radiusChanged(_ sender: UISlider) {
+        applyProcessing()
+    }
+    
+    
+    
     @IBAction func changeFilter(_ sender: Any) {
         let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
         ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
@@ -65,20 +78,32 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @IBAction func save(_ sender: UIButton) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            // Challenge_1:
+            print("No image")
+            let ac = UIAlertController(title: "No image", message: "You can't save a photo that doesn't exist", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .cancel))
+            present(ac, animated: true)
+            return
+            
+        }
 
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
+    
+    
     
     func applyProcessing() {
         let inputKeys = currentFilter.inputKeys
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(radius.value * 200, forKey: kCIInputRadiusKey) }
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
         if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
-
-        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: currentFilter.outputImage!.extent) {
             let processedImage = UIImage(cgImage: cgimg)
             self.imageView.image = processedImage
         }
@@ -90,7 +115,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
 
         // safely read the alert action's title
         guard let actionTitle = action.title else { return }
-
+        // Challenge_2:
+        chageFilter.setTitle("Change Filter (\(actionTitle))", for: .normal)
+        //title = actionTitle
+        
         currentFilter = CIFilter(name: actionTitle)
 
         let beginImage = CIImage(image: currentImage)

1ca97b388261b48daead07158c6d59c367c7450a
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-06-24 22:50:25 +0300

    Add sound

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -38,7 +38,8 @@ class WhackSlot: SKNode {
     
     func show(hideTime: Double) {
         if isVisible { return }
-
+        charNode.xScale = 1
+        charNode.yScale = 1
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
@@ -61,6 +62,15 @@ class WhackSlot: SKNode {
 
         charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
         isVisible = false
+    }
+    
+    func hit() {
+        isHit = true
+
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [unowned self] in self.isVisible = false }
+        charNode.run(SKAction.sequence([delay, hide, notVisible]))
     }
     
     

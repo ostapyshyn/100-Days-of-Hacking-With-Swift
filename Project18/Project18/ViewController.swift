f5777acd3d94c962d0fb3141869666c9307e8319
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-05 22:19:59 +0300

    Finish Day 64

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Volodymyr Ostapyshyn on 04.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+        
+    }
+
+
+}
+

57a7205428496da7bc6a511122fb71f98d6caf63
Volodymyr Ostapyshyn <ostapyshyn@gmail.com>
2020-07-07 22:13:03 +0300

    Finish Day 66

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Volodymyr Ostapyshyn on 07.07.2020.
+//  Copyright © 2020 Volodymyr Ostapyshyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
